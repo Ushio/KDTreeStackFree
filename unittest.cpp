1ae51ac8f42bbf322981f02b4bf96f4755751d6a
ushiostarfish <ushiostarfish@gmail.com>
2025-07-10 14:26:23 +0900

    measurements

--- unittest.cpp
+++ unittest.cpp
@@ -189,3 +189,153 @@ TEST_CASE("tree", "") {
     printf("nanoflann %f\n", nanoflann_time);
     printf("stackfree %f\n", stackfree_time);
 }
+
+TEST_CASE("comparison", "") 
+{
+    using namespace pr;
+
+    enum 
+    {
+        dims = 2
+    };
+    const int mQuery = 1000 * 1000;
+    const int N = 1000 * 10;
+    //const float radius = 0.01f;
+    PCG rng;
+
+    uint32_t g_hash = 0;
+
+    printf("stackful stackless nanoflann\n");
+    
+    for (float radius : {0.01f, 0.02f, 0.04f})
+    {
+        std::vector<kdtree::VecN<dims>> points;
+
+        PointCloud<dims> cloud;
+
+        for (int j = 0; j < N; j++)
+        {
+            kdtree::VecN<dims> p;
+            for (int k = 0; k < dims; k++)
+            {
+                p[k] = rng.uniformf();
+            }
+            cloud.points.push_back(p);
+        }
+
+        // nanoflann
+        using kdAdapter = nanoflann::KDTreeSingleIndexAdaptor<
+            nanoflann::L2_Simple_Adaptor<float, PointCloud<dims>>,
+            PointCloud<dims>, dims /* dim */
+        >;
+
+        kdAdapter kdtree(dims /*dim*/, cloud, { 10 /* max leaf */ });
+
+        // kdtree
+        std::vector<kdtree::Node<dims>> nodes(kdtree::storage_size(cloud.points.size()));
+        kdtree::build(nodes.data(), cloud.points.data(), cloud.points.size());
+
+        { // validation
+            for (int j = 0; j < mQuery; j++)
+            {
+                kdtree::VecN<dims> query_pt;
+                for (int k = 0; k < dims; k++)
+                {
+                    query_pt[k] = rng.uniformf();
+                }
+
+                uint32_t ref_hash = 0;
+                {
+                    static std::vector<nanoflann::ResultItem<uint32_t, float>> ret_matches;
+
+                    size_t nMatches = kdtree.radiusSearch(query_pt.xs, radius * radius, ret_matches);
+                    for (size_t i = 0; i < nMatches; i++)
+                    {
+                        uint32_t index = ret_matches[i].first;
+                        ref_hash ^= index;
+                    }
+                }
+                {
+                    uint32_t my_hash = 0;
+                    {
+                        kdtree::radius_query(nodes.data(), cloud.points.size(), query_pt, radius, [&my_hash](uint32_t index) {
+                            my_hash ^= index;
+                        });
+                    }
+                    REQUIRE(my_hash == ref_hash);
+                }
+                {
+                    uint32_t my_hash = 0;
+                    {
+                        kdtree::radius_query_stackful(nodes.data(), cloud.points.size(), query_pt, radius, [&my_hash](uint32_t index) {
+                            my_hash ^= index;
+                        });
+                    }
+                    REQUIRE(my_hash == ref_hash);
+                }
+            }
+
+        }
+
+
+        {
+            Stopwatch sw;
+            PCG rng_pt;
+            for (int j = 0; j < mQuery; j++)
+            {
+                kdtree::VecN<dims> query_pt;
+                for (int k = 0; k < dims; k++)
+                {
+                    query_pt[k] = rng_pt.uniformf();
+                }
+
+                kdtree::radius_query_stackful(nodes.data(), cloud.points.size(), query_pt, radius, [&g_hash](uint32_t index) {
+                    g_hash ^= index;
+                });
+            }
+            printf("%.6f ", sw.elapsed());
+        }
+        {
+            Stopwatch sw;
+            PCG rng_pt;
+            for (int j = 0; j < mQuery; j++)
+            {
+                kdtree::VecN<dims> query_pt;
+                for (int k = 0; k < dims; k++)
+                {
+                    query_pt[k] = rng_pt.uniformf();
+                }
+
+                kdtree::radius_query(nodes.data(), cloud.points.size(), query_pt, radius, [&g_hash](uint32_t index) {
+                    g_hash ^= index;
+                });
+            }
+            printf("%.6f ", sw.elapsed());
+        }
+        {
+            Stopwatch sw;
+            PCG rng_pt;
+            for (int j = 0; j < mQuery; j++)
+            {
+                kdtree::VecN<dims> query_pt;
+                for (int k = 0; k < dims; k++)
+                {
+                    query_pt[k] = rng_pt.uniformf();
+                }
+
+                static std::vector<nanoflann::ResultItem<uint32_t, float>> ret_matches;
+
+                size_t nMatches = kdtree.radiusSearch(query_pt.xs, radius * radius, ret_matches);
+                for (size_t i = 0; i < nMatches; i++)
+                {
+                    uint32_t index = ret_matches[i].first;
+                    g_hash ^= index;
+                }
+            }
+            printf("%.6f ", sw.elapsed());
+        }
+
+        printf("\n");
+    }
+    printf("%d\n", g_hash);
+}
